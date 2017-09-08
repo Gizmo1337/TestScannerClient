9f58e0863ed56fb2037e825ef4021f7885760878
Victor Cherescu <gabriel.victor1337@gmail.com>
2017-09-08 17:59:01 +0300

    Angular SetUp + Table SetUp with 4 columns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import Handsontable from 'handsontable';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  data: any[];
+  columns: object[] = [
+    {data: 'id', title: 'Id'},
+    {data: 'airdate', title: 'AirDate'},
+    {data: 'airtime', title: 'AirTime'},
+    {data: 'name', title: 'Name', width: 200},
+    {data: 'number', title: 'Number'},
+    {data: 'runtime', title: 'Runtime'},
+    {data: 'season', title: 'Season'},
+    {data: 'summary', title: 'Summary', renderer: 'html', width: 800},
+  ];
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HotTableModule } from 'angular-handsontable/index';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HotTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
